b88f424715ca33c7b8d2357405718d4cf969cd4b
Elena Mikirtumova <mikirtumova84@gmail.com>
2022-10-05 23:31:42 +0400

    add solution for task 3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  num1: undefined,
+  num2: undefined,
+  read(a, b) {
+    this.num1 = Number(a);
+    this.num2 = Number(b);
+  },
+  sum() {
+    return this.num1 + this.num2;
+  },
+  mul() {
+    return this.num1 * this.num2;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
