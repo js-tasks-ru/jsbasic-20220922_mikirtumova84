208cf308e21d668f7a891457ea794f738437c8b4
Elena Mikirtumova <mikirtumova84@gmail.com>
2022-10-09 23:16:28 +0400

    add solution fot task 3 module 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
 function camelize(str) {
-  // ваш код...
+  let firstWord = str.split('-').slice(0, 1);
+
+  let lastWords = str.split('-').slice(1).map(item => item[0].toUpperCase() + item.slice(1)).join('');
+
+  return firstWord + lastWords;
 }
