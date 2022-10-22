0a0422253337808ad18bae43d2c12d2d95c527fb
Elena Mikirtumova <mikirtumova84@gmail.com>
2022-10-22 23:20:08 +0400

    add solution for task 1 module 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,50 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.renderElem();
+  }
+
+  renderElem() {
+    const table = document.createElement('table');
+
+    const tableHeader = document.createElement('thead');
+    tableHeader.innerHTML = `
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>`;
+
+    table.appendChild(tableHeader);
+
+    const tableBody = document.createElement('tbody');
+    tableBody.innerHTML = this.rows.map(tableRow => `
+      <tr>
+        <td>${tableRow.name}</td>
+        <td>${tableRow.age}</td>
+        <td>${tableRow.salary}</td>
+        <td>${tableRow.city}</td>
+        <td><button type="button">X</button></td>
+      </tr>
+    `).join('');
+
+    table.appendChild(tableBody);
+
+    this.table = table;
+
+    this.onClick();
+
+    return table;
+  }
+
+  onClick() {
+    this.table.addEventListener('click', (evt) => {
+      if (evt.target.closest('button')) {
+        evt.target.closest('tr').remove();
+      }
+    });
   }
 }
